4d42bd20f4f57093e8453422336481cd33fa0b5c
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2025-04-16 06:36:43 -0700

    Create doMath.h

---
+++ doMath.h
@@ -0,0 +1,19 @@
+void add(int n1, int n2) {
+	int addNum = n1 + n2;
+	printf("%d + %d = %d", n1, n2, addNum);
+}
+
+void sub(int n1, int n2) {
+	int subNum = n1 - n2;
+	printf("%d - %d = %d", n1, n2, subNum);
+}
+
+void div(int n1, int n2) {
+	int divNum = n1 / n2;
+	printf("%d / %d = %d", n1, n2, divNum);
+}
+
+void mul(int n1, int n2) {
+	int mulNum = n1 * n2;
+	printf("%d * %d = %d", n1, n2, mulNum);
+}
